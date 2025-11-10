4945e605d49baa7857ac348d58c37d45ca8c8021
rayyank06 <rayyan.khan1@ontariotechu.net>
2025-11-09 22:30:17 -0500

    Finished the final touches

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Main Express Application Configuration
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,37 +6,37 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var expressLayouts = require('express-ejs-layouts');
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(expressLayouts);
+// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use('/fontawesome', express.static(path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free')));
 
-// catch 404 and forward to error handler
+// Use routes
+app.use('/', indexRouter);
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { title: 'Error' });
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
+// Routes for Portfolio Website
+// This file defines all the routes (URLs) for our Express application
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Home',
+    body: '' 
+  });
+});
+
+/* GET about page */
+router.get('/about', function(req, res, next) {
+
+  res.render('about', { 
+    title: 'About Me'
+  });
+});
+
+
+router.get('/projects', function(req, res, next) {
+ 
+  res.render('projects', { 
+    title: 'Projects'
+  });
+});
+
+/* GET contact page */
+router.get('/contact', function(req, res, next) {
+ 
+  res.render('contact', { 
+    title: 'Contact'
+  });
+});
+
+
+router.post('/contact', function(req, res, next) {
+  
+  console.log('Form submitted:');
+  console.log('Name:', req.body.name);
+  console.log('Email:', req.body.email);
+  console.log('Phone:', req.body.phone);
+  console.log('Comments:', req.body.comments);
+  
+
+  res.redirect('/contact');
 });
 
 module.exports = router;
